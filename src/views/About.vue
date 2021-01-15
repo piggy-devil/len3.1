b50f8544e5ccf8e6609eaf642d26c26ef2902c35
piggy-devil <muupaza@gmail.com>
2021-01-15 11:10:38 +0700

    f

---
+++ src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <table class="border-separate border border-green-800 ...">
+  <thead>
+    <tr>
+      <th class="border border-green-600 ...">State</th>
+      <th class="border border-green-600 ...">City</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="border border-green-600 ...">Indiana</td>
+      <td class="border border-green-600 ...">Indianapolis</td>
+    </tr>
+    <tr>
+      <td class="border border-green-600 ...">Ohio</td>
+      <td class="border border-green-600 ...">Columbus</td>
+    </tr>
+    <tr>
+      <td class="border border-green-600 ...">Michigan</td>
+      <td class="border border-green-600 ...">Detroit</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</template>
